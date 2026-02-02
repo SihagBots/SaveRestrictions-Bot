023a7ba396db53fe314bce8a677b8e651e1cd32a
NeonAnurag <justasneon@gmail.com>
2026-02-02 20:46:44 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -28,6 +28,9 @@ DB_NAME = os.environ.get("DB_NAME", "SaveRestricted")
 # Log Channel to Track New Users 
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001889915480"))
 
+# Dump Channel for File Tracking (ADDED)
+DUMP_CHANNEL = int(os.environ.get("DUMP_CHANNEL", "-1002766188813"))
+
 # If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then False
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
 
